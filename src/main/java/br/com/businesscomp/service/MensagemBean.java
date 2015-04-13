97822d51344db7778b818b966d32a0049d8f3a0a
Alexandre <correaasouza@gmail.com>
2015-04-13 20:17:55 -0300

    ajustes

--- src/main/java/br/com/businesscomp/service/MensagemBean.java
+++ src/main/java/br/com/businesscomp/service/MensagemBean.java
@@ -23,47 +23,47 @@ public class MensagemBean {
 public String getHorario() {
         
     String databaseUrl = System.getenv("DATABASE_URL");
-    
-    StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
-    String dbVendor = st.nextToken(); //if DATABASE_URL is set
-    String userName = st.nextToken();
-    String password = st.nextToken();
-    String host = st.nextToken();
-    String port = st.nextToken();
-    String databaseName = st.nextToken();
-    String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
-    Map<String, String> properties = new HashMap<String, String>();
-    properties.put("javax.persistence.jdbc.url","jdbc:postgresql://localhost:5432/businesscomp");
-    properties.put("javax.persistence.jdbc.user", userName);
-    properties.put("javax.persistence.jdbc.password", password);
-    properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-    properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");   
-    
-    
-//    properties.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:5432/businesscomp");
-//    properties.put("javax.persistence.jdbc.user", "postgres" );
-//    properties.put("javax.persistence.jdbc.password", "password" );
-//    properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-//    properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-    
-   
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory("businesscomp", properties);
 //    
-//	//EntityManagerFactory factory = Persistence.createEntityManagerFactory("businesscomp");
-	EntityManager manager = factory.createEntityManager();
-	JpaTest test = new JpaTest(manager);
-	
-
-	EntityTransaction tx = manager.getTransaction();
-	tx.begin();
-	try {
-		test.createEmployees();
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-	tx.commit();
-
-	test.listEmployees();
+//    StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+//    String dbVendor = st.nextToken(); //if DATABASE_URL is set
+//    String userName = st.nextToken();
+//    String password = st.nextToken();
+//    String host = st.nextToken();
+//    String port = st.nextToken();
+//    String databaseName = st.nextToken();
+//    String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
+//    Map<String, String> properties = new HashMap<String, String>();
+//    properties.put("javax.persistence.jdbc.url","jdbc:postgresql://localhost:5432/businesscomp");
+//    properties.put("javax.persistence.jdbc.user", userName);
+//    properties.put("javax.persistence.jdbc.password", password);
+//    properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+//    properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");   
+//    
+//    
+////    properties.put("javax.persistence.jdbc.url", "jdbc:postgresql://localhost:5432/businesscomp");
+////    properties.put("javax.persistence.jdbc.user", "postgres" );
+////    properties.put("javax.persistence.jdbc.password", "password" );
+////    properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+////    properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//    
+//   
+//    EntityManagerFactory factory = Persistence.createEntityManagerFactory("businesscomp", properties);
+////    
+////	//EntityManagerFactory factory = Persistence.createEntityManagerFactory("businesscomp");
+//	EntityManager manager = factory.createEntityManager();
+//	JpaTest test = new JpaTest(manager);
+//	
+//
+//	EntityTransaction tx = manager.getTransaction();
+//	tx.begin();
+//	try {
+//		test.createEmployees();
+//	} catch (Exception e) {
+//		e.printStackTrace();
+//	}
+//	tx.commit();
+//
+//	test.listEmployees();
 
 	SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
 	System.out.println(".. done");
