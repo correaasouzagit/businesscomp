4935bff95964ed40dc522b5461df22821b2d0c09
Alexandre <correaasouza@gmail.com>
2015-04-13 21:11:50 -0300

    ajuste

--- src/main/java/br/com/businesscomp/service/MensagemBean.java
+++ src/main/java/br/com/businesscomp/service/MensagemBean.java
@@ -38,9 +38,12 @@ public class MensagemBean {
 		String databaseName = st.nextToken();
 		String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port, databaseName);
 		Map<String, String> properties = new HashMap<String, String>();
-		properties.put("javax.persistence.jdbc.url",databaseUrl);
+		properties.put("javax.persistence.jdbc.url",jdbcUrl);
+		System.out.println("databaseUrl: " + jdbcUrl );
 		properties.put("javax.persistence.jdbc.user", userName);
+		System.out.println("userName: " + userName);
 		properties.put("javax.persistence.jdbc.password", password);
+		System.out.println("password: " + password);
 		properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
 		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");   
 		
@@ -50,11 +53,11 @@ public class MensagemBean {
 //		properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
 //		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 
-		properties.put("javax.persistence.jdbc.url","jdbc:postgresql://ec2-54-163-235-165.compute-1.amazonaws.com:5432/d7u3nt4ag4434e");
-		properties.put("javax.persistence.jdbc.user", "zezmhrpxgmbcnr");
-		properties.put("javax.persistence.jdbc.password", "EMtOPnLrOJg-bSwwcgpnH1JsDj");
-		properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//		properties.put("javax.persistence.jdbc.url","jdbc:postgresql://ec2-54-163-235-165.compute-1.amazonaws.com:5432/d7u3nt4ag4434e");
+//		properties.put("javax.persistence.jdbc.user", "zezmhrpxgmbcnr");
+//		properties.put("javax.persistence.jdbc.password", "EMtOPnLrOJg-bSwwcgpnH1JsDj");
+//		properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+//		properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		
 		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("businesscomp", properties);
